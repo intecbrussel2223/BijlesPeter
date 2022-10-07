56141cf320328ee6dc231fd9ec4efccc7ec6bd99
kenan kurda <kenankurda@gmail.com>
2022-10-07 10:58:10 +0200

    ok

---
+++ Modulo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 7; i++)
+            {
+                if (i % 5 == 0)
+                {
+                    Console.WriteLine($"{i} zonder rest");
+                }
+                else
+                {
+                    Console.WriteLine($"de rest waarde van i is {i % 5}");
+                }
+            }
+        }
+    }
+}
---
+++ Rufat/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rufat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("--------------------------------");
+
+            int x = 6;
+            while (x <6)
+            {
+                Console.WriteLine(x);
+                x++;
+            }
+
+
+
+            
+        }
+    }
+}
