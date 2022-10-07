d562196285e718eba4f814b9f5799bd601bde2df
kenan kurda <kenankurda@gmail.com>
2022-10-07 10:06:05 +0200

    Add project files.

---
+++ BijlesPeter/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BijlesPeter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string voorNaam;
+            int leeftijd;
+            int geboorteJaar;
+            Console.Write("Geef uw naam aub ");
+            voorNaam = Console.ReadLine();
+            Console.WriteLine($"Uw naam is {voorNaam}");
+            Console.Write("Wat is uw geboortejaar?");
+            geboorteJaar = int.Parse(Console.ReadLine());
+            leeftijd = 2022 - geboorteJaar;
+            Console.WriteLine($"uw leeftijd is {leeftijd}");
+        }
+    }
+}
---
+++ ProductVerkoop/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductVerkoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string product;
+
+            decimal prijs;
+            decimal btw;
+            decimal prijsMetBtw;
+            decimal btwWaarde;
+
+            Console.Write("Geef de product naam aub ");
+            product = Console.ReadLine();
+            Console.Write("Wat is de prijs ");
+            prijs = decimal.Parse(Console.ReadLine());
+            Console.Write("Geef de btw in aub ");
+            btw = decimal.Parse(Console.ReadLine());
+            btwWaarde = prijs * btw /100;
+            prijsMetBtw = btwWaarde + prijs;
+            Console.WriteLine($"{product} de prijs zonder btw" +
+                $"is {prijs} + btw {btwWaarde} = {prijsMetBtw}");
+
+            
+
+
+        }
+    }
+}
